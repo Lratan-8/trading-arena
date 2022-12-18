8d298f9a84d408c09b7da17369a92e77613814ad
Luv <luv.ratan@indegene.com>
2022-12-18 12:35:16 +0530

    navbar added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
